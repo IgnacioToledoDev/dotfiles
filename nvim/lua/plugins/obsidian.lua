5d2ebd29a27007d556f162cde53f2c8606d98270
Ignacio Toledo <igna.toledot@gmail.com>
2026-05-20 18:38:19 -0400

    feat(nvim): add obisidian plugin

---
+++ nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "nova",
+          path = "~/Documents/nova",
+        },
+      },
+      notes_subdir = "notas",
+      new_notes_location = "notes_subdir",
+      completion = {
+        nvim_cmp = true,
+        min_chars = 2,
+      },
+      mappings = {
+        ["gf"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        ["<leader>ch"] = {
+          action = function()
+            return require("obsidian").util.toggle_checkbox()
+          end,
+          opts = { buffer = true },
+        },
+        ["<cr>"] = {
+          action = function()
+            return require("obsidian").util.smart_action()
+          end,
+          opts = { buffer = true, expr = true },
+        },
+      },
+      ui = {
+        enable = true,
+        checkboxes = {
+          [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+          ["x"] = { char = "", hl_group = "ObsidianDone" },
+        },
+      },
+    },
+  },
+}
